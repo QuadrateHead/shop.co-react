ec91c205db70b1f290a369474e5e69a2fe8b29dc
QuadrateHead <korcinskiy.ivan@gmail.com>
2026-07-04 14:34:16 +0200

    Prodect Reviews Part

--- src/components/ProductReviews/ProductReviews.jsx
+++ src/components/ProductReviews/ProductReviews.jsx
@@ -3,7 +3,24 @@ import React, { useState } from 'react'
 const ProductReviews = () => {
 
   return (
-    <div>ProductReviews</div>
+    <div className='prdctreviews'>
+      <div className="prdctreviews__header">
+        <div className="prdctreviews__allreviews">
+          <h2 className="prdctreviews__allreviews-heading"></h2>
+          <p className="prdctreviews__allreviews-count"></p>
+        </div>
+        <div className="prdctreviews__conf">
+          <div className="prdctreviews__filter"></div>
+          <select className="prdctreviews__sort">
+            <option value="latest">Latest</option>
+            <option value="highest">Highest Rated</option>
+            <option value="lowest">Lowest Rated</option>
+            <option value="helpful">Most Helpful</option>
+          </select>
+          <button className="prdctreviews__write-btn"></button>
+        </div>
+      </div>
+    </div>
   )
 }
 
