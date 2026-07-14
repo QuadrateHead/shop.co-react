5811f0761f733af73f4abe1cfeb5938d07283982
QuadrateHead <korcinskiy.ivan@gmail.com>
2026-07-14 19:49:56 +0200

    Create BurgerSide

---
+++ src/elements/BurgerSide/BurgerSide.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import styles from "./BurgerSide.module.scss"
+const BurgerSide = () => {
+  return (
+    <div className={styles.block}>
+      
+    </div>
+  )
+}
+
+export default BurgerSide
